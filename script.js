b3c27cd28ff1287384bf5a69811d1d70136c806e
Anmol Vishwakarma <anmol2023@ipsacademy.org>
2022-03-20 17:56:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const min = document.querySelector(".min .numb");
 const sec = document.querySelector(".sec .numb");
 var timer = setInterval(() => {
     var currentDate = new Date().getTime();
-    var launchDate = new Date('march 18, 2022 13:00:00').getTime();
+    var launchDate = new Date('march 31, 2022 13:00:00').getTime();
     var duration = launchDate - currentDate;
     var days = Math.floor(duration / (1000 * 60 * 60 * 24));
     var hours = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
